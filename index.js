c7502649f71212eb7d7e4b886bcf91cc0c635b19
Cole Willsea <coleww@gmail.com>
2015-05-30 11:38:05 -0700

    simplify

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var format = require('json-nice');
 
-var body = document.querySelector("body");
 var output = document.querySelector(".formatted");
 
-body.addEventListener('paste', function(e){
+document.body.addEventListener('paste', function(e){
   var data = e.clipboardData.getData('text/plain');
   var json = JSON.parse(data);
   var fmtJson = format(json);
